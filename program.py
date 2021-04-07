99213e3c78101ad6552f9a99ce760e186a294018
jameel harris <ceo@jameelharris.com>
2021-04-07 17:45:14 -0500

    Mobile layout code change
    Mobile layout code change

--- program.py
+++ program.py
@@ -15,7 +15,9 @@ from dash.exceptions import PreventUpdate
 
 import base64
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ])
 app.layout = html.Div([
 
     html.Div([        
